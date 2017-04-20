d6770ada17f8789fc7e67dd4a2802804abecc5eb
CharlesKasper <7ckasper@stpatrick.org>
2017-04-20 10:48:01 -0500

    Add files via upload

---
+++ bbcomeback.js
@@ -0,0 +1,8 @@
+const doc=doc;
+let section=doc.querySelector("section img")
+for (var i = 0; i < section.length; i++) {
+    section[i].addEventListener("hover")
+    function functionName() {
+
+    }
+}
